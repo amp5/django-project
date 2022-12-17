6cf602072068821e7aae07cd7ce402012a40c235
Alexandra Plassaras <alexandra.plassaras@rover.com>
2022-12-17 11:31:13 -0800

    fix list_view

--- blogging/views.py
+++ blogging/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http.response import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
 
@@ -17,7 +17,7 @@ def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
     context = {'posts': posts}
-    return render(request, 'list.html', context)
+    return render(request, 'blogging/list.html', context)
 
 def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
