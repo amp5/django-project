184cd5f7719176f70aaf67b14239c19639be5bd0
Alexandra Plassaras <alexandra.plassaras@rover.com>
2022-12-17 17:04:42 -0800

    working code

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,20 +1,31 @@
+# from django.contrib import admin
+# from blogging.models import Post, Category
+#
+# class CategoryAdmin(admin.ModelAdmin):
+#     exclude = ('Posts',)
+#
+# class CategoryInline(admin.TabularInline):
+#     model = Category
+#
+# class PostAdmin(admin.ModelAdmin):
+#     inlines = [CategoryInline,]
+#
+#
+# admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Post, PostAdmin)
+
+
+
 from django.contrib import admin
 from blogging.models import Post, Category
-
+# and a new admin registration
 class CategoryAdmin(admin.ModelAdmin):
-    exclude = ('posts',)
-
-
-admin.site.register(Category, CategoryAdmin)
-
-
-class CategoryInline(admin.TabularInline):
+    exclude = ('Posts',)
+class CategoryInLine(admin.TabularInline):
     model = Category
-
-
 class PostAdmin(admin.ModelAdmin):
-    inlines = [CategoryInline]
-
-
-
+    inlines = [
+        CategoryInLine,
+    ]
 admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
